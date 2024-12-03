dae49279f72653371272b62a9c322232f7b8a5e0
carlos <carlosleonfranco13@gmail.com>
2024-12-03 11:28:10 -0500

    S22-Inicio-Pantalla de confirmación1

--- src/app/(shop)/checkout/(checkout)/page.tsx
+++ src/app/(shop)/checkout/(checkout)/page.tsx
@@ -1,15 +1,8 @@
 
 import { Title } from "@/components";
-import { initialData } from "@/seed/seed";
-import Image from "next/image";
 import Link from "next/link";
+import { ProductsInCart } from "./ui/ProductsInCart";
 
-
-const productsInCart = [
-  initialData.products[0],
-  initialData.products[1],
-  initialData.products[2],
-];
 
 export default function CheckoutPage() {
   return (
@@ -29,31 +22,7 @@ export default function CheckoutPage() {
             </Link>
 
             {/* Items */}
-            {
-              productsInCart.map(product => (
-                <div key={product.slug} className="flex mb-5">
-
-                  <Image
-                    src={`/products/${product.images[0]}`}
-                    width={100}
-                    height={100}
-                    style={{
-                      width:'100px',
-                      height: '100px'
-                    }}
-                    alt={product.title}
-                    className="mr-5 rounded"
-                  />
-
-                  <div>
-                    <p>{product.title}</p>
-                    <p>${product.price} x 3</p>
-                    <p className="font-bold">Subtotal: ${product.price * 3}</p>
-                  </div>
-
-                </div>
-              ))
-            }
+            <ProductsInCart/>
           </div>
 
 
