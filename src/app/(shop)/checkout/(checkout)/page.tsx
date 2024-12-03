930309bf44dcb534c1dc0bc3281ca1ead1c73056
carlos <carlosleonfranco13@gmail.com>
2024-12-03 12:05:34 -0500

    S22-Inicio-Pantalla de confirmación2

--- src/app/(shop)/checkout/(checkout)/page.tsx
+++ src/app/(shop)/checkout/(checkout)/page.tsx
@@ -2,6 +2,7 @@
 import { Title } from "@/components";
 import Link from "next/link";
 import { ProductsInCart } from "./ui/ProductsInCart";
+import { PlaceOrder } from "./ui/PlaceOrder";
 
 
 export default function CheckoutPage() {
@@ -27,61 +28,7 @@ export default function CheckoutPage() {
 
 
           {/* Checkout - Resumen de Compra */}
-          <div className="bg-white rounded-xl shadow-xl p-7">
-
-            <h2 className="text-2xl mb-2">Dirección de Entrega</h2>
-            <div className="mb-10">
-              <p className="text-xl">Fernando Herrera</p>
-              <p>Av. Siempre Viva 123</p>
-              <p>Colonia Cnetor</p>
-              <p>Alcaldía Cuauthemoc</p>
-              <p>Ciudad de México</p>
-              <p>CP. 2765892</p>
-              <p>5765 876876 86678</p>
-            </div>
-
-            {/* Divider */}
-            <div
-              className="w-full h-0.5 rounded bg-gray-200 mb-10"
-            />
-
-            <h2 className="text-2xl mb-2"> Resumen de Orden </h2>
-
-            <div className="grid grid-cols-2">
-
-              <span>No.Productos</span>
-              <span className="text-right">3 Artículos</span>
-
-              <span>Subtotal</span>
-              <span className="text-right">$100</span>
-
-              <span>Impuestos (15%)</span>
-              <span className="text-right">$100</span>
-
-              <span className="mt-5 text-2xl">Total:</span>
-              <span className="mt-5 text-2xl text-right">$100</span>
-
-            </div>
-
-            <div className="mt-5 mb-2 w-full">
-
-              {/* Disclaimer */}
-              <p className="mb-5">
-                <span className="text-xs">
-                  Al hacer click en &quot;Colocar Orden&quot;, aceptas nuestros <a href="#" className="underline">términos y condiciones</a> y <a href="#" className="underline">política de privacidad</a>
-                </span>
-              </p>
-
-              <Link 
-                className="flex btn-primary justify-center"
-                href="/orders/123"
-              >
-                Colocar Orden
-              </Link>
-
-            </div>
-
-          </div>
+          <PlaceOrder/>
 
         </div>
 
