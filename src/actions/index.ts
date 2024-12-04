37d137f4f6d7277b24b214d0dcc8390c000483ba
carlos <carlosleonfranco13@gmail.com>
2024-12-04 17:07:22 -0500

    S23-ServerAction-Verificar pago-Pt1

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -17,6 +17,7 @@ export * from './order/get-order-by-id'
 export * from './order/get-orders-by-user'
 
 export * from './payments/set-transaction-id'
+export * from './payments/paypal-check-payment'
 
 export * from './product/get-product-by-slug'
 export * from './product/get-stock-by-slug'
