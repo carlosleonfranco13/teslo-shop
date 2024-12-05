c85725d871f3ed0062be48632d6bb3dd79bed71a
carlos <carlosleonfranco13@gmail.com>
2024-12-05 08:33:03 -0500

    S24-Visor de Ordenes

--- src/components/paypal/PayPalButton.tsx
+++ src/components/paypal/PayPalButton.tsx
@@ -60,9 +60,12 @@ export const PayPalButton = ({orderId, amount}: Props) => {
 
 
     return (
-        <PayPalButtons 
+        <div className="relative z-0">
+            <PayPalButtons 
             createOrder={createOrder}
             onApprove={onApprove}
         />
+        </div>
+        
     )
 }
