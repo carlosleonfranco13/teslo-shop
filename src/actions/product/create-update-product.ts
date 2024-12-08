53f97b5a019aba6fcb80d72addc6ce88be1bebd0
carlos <carlosleonfranco13@gmail.com>
2024-12-07 20:43:30 -0500

    S24-Invalidar Paths y Redireccionar

--- src/actions/product/create-update-product.ts
+++ src/actions/product/create-update-product.ts
@@ -2,7 +2,8 @@
 
 import prisma from '@/lib/prisma';
 import { Gender, Product, Size } from '@prisma/client';
-import {z} from 'zod';
+import { revalidatePath } from 'next/cache';
+import { z } from 'zod';
 
 const productSchema = z.object({
     id: z.string().uuid().optional().nullable(),
@@ -12,79 +13,97 @@ const productSchema = z.object({
     price: z.coerce
         .number()
         .min(0)
-        .transform( val => Number(val.toFixed(2)) ),
+        .transform(val => Number(val.toFixed(2))),
     inStock: z.coerce
         .number()
         .min(0)
-        .transform( val => Number(val.toFixed(0)) ),
+        .transform(val => Number(val.toFixed(0))),
     categoryId: z.string().uuid(),
-    sizes: z.coerce.string().transform( val => val.split(',') ),
+    sizes: z.coerce.string().transform(val => val.split(',')),
     tags: z.string(),
     gender: z.nativeEnum(Gender)
 })
 
-export const createUpdateProduct = async(formData: FormData) => {
+export const createUpdateProduct = async (formData: FormData) => {
 
     const data = Object.fromEntries(formData);
-    const productParsed = productSchema.safeParse( data );
+    const productParsed = productSchema.safeParse(data);
 
-    if(!productParsed.success) {
+    if (!productParsed.success) {
         console.log(productParsed.error);
-        return {ok: false}
-    } 
+        return { ok: false }
+    }
 
     const product = productParsed.data;
     product.slug = product.slug.toLowerCase().replace(/ /g, '-').trim();
 
-    const {id, ...rest} = product;
+    const { id, ...rest } = product;
 
-    const prismaTx = await prisma.$transaction( async (tx) => {
+    try {
+        const prismaTx = await prisma.$transaction(async (tx) => {
 
-        let product: Product;
-        const tagsArray = rest.tags.split(',').map( tag => tag.trim().toLowerCase())
+            let product: Product;
+            const tagsArray = rest.tags.split(',').map(tag => tag.trim().toLowerCase())
 
-        if(id) {
-            // Actualizar
-            product = await prisma.product.update({
-                where: {id},
-                data: {
-                    ...rest,
-                    sizes: {
-                        set: rest.sizes as Size[],
-                    },
-                    tags: {
-                        set: tagsArray
+            if (id) {
+                // Actualizar
+                product = await prisma.product.update({
+                    where: { id },
+                    data: {
+                        ...rest,
+                        sizes: {
+                            set: rest.sizes as Size[],
+                        },
+                        tags: {
+                            set: tagsArray
+                        }
                     }
-                }
-            });
+                });
 
 
-        } else {
-            // Crear
-            product = await prisma.product.create({
-                data: {
-                    ...rest,
-                    sizes: {
-                        set: rest.sizes as Size[],
-                    },
-                    tags: {
-                        set: tagsArray
+            } else {
+                // Crear
+                product = await prisma.product.create({
+                    data: {
+                        ...rest,
+                        sizes: {
+                            set: rest.sizes as Size[],
+                        },
+                        tags: {
+                            set: tagsArray
+                        }
                     }
-                }
-            })
+                })
+            }
+
+            console.log({ product });
+
+
+            return {
+                product
+            }
+        });
+
+        // TODO: RevalidatePaths
+        revalidatePath('/admin/products');
+        revalidatePath(`/admin/product/${ product.slug }`)
+        revalidatePath(`/products/${ product.slug }`)
+
+        return {
+            ok: true,
+            product: prismaTx.product,
         }
 
-        console.log({ product}); 
 
+    } catch (error) {
 
-        return {    
-            product
+        return {
+            ok: false,
+            message: 'Revisar los Logs, no se pudo actualizar/Crear'
         }
-    });
-
-    // TODO: RevalidatePaths
-
-    return {
-        ok: true, 
     }
+
+
+
+
 }
